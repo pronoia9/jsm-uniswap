0388518c12ed857c4327136181b725d8d94f218b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 00:33:33 +0300

    add logo

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,20 @@
-import React from "react"
+import React from 'react';
+import { useEthers } from '@usedapp/core';
+
+import styles from './styles';
+import { uniswapLogo } from './assets';
 
 const App = () => {
-  return <p>Hello, Web3!</p>
-}
+  return (
+    <div className={styles.container}>
+      <div className={styles.innerContainer}>
+        <header className={styles.header}>
+          {/* Navbar */}
+          <img src={uniswapLogo} alt='uniswap-logo' className='w-16 h-16 object-contain' />
+        </header>
+      </div>
+    </div>
+  );
+};
 
 export default App;
