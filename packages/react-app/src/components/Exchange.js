7232116a9c3ea2ad82f9e932319854cd2298f3bd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 02:16:35 +0300

    add layout

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -7,11 +7,18 @@ import { ethers } from 'ethers';
 import { parseUnits } from 'ethers/lib/utils';
 
 import { ROUTER_ADDRESS } from '../config';
+import { AmountIn, AmountOut, Balance } from '../components';
 
 const Exchange = ({ pools }) => {
   return (
     <div className='flex flex-col w-full items-center'>
       <div className='mb-8'>
+        <AmountIn />
+        <Balance />
+      </div>
+      <div className='mb-8 w-[100%]'>
+        <AmountOut />
+        <Balance />
       </div>
     </div>
   );
