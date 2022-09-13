88176914dec431feaeeaf1b8643ea144c17214bb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 02:36:48 +0300

    add initial balance

--- packages/react-app/src/components/Balance.js
+++ packages/react-app/src/components/Balance.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { formatUnits, parseUnits } from 'ethers/lib/utils';
+
+import styles from '../styles';
 
 const Balance = () => {
-  return <div>Balance</div>;
+  // !temp
+  const tokenBalance = parseUnits('0.00251');
+
+  return (
+    <div className={styles.balance}>
+      <p className={styles.balanceText}>
+        {tokenBalance && (
+          <span className={styles.balanceBold}>Balance: {formatUnits(tokenBalance || parseUnits('0'))}</span>
+        )}
+      </p>
+    </div>
+  );
 };
 
 export default Balance;
