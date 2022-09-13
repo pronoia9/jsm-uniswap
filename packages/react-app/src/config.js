d2a6efa6711da7c7923f1e126cd3e550d24cfdb9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 01:07:34 +0300

    add addresses

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,10 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = '0xAEe67Eb024B91A22Fbf595110Ce88C0715DC8FdD';
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/i98zBA0AiUQFxM48eKMTN04p8a-Hx8EZ",
   },
 };
