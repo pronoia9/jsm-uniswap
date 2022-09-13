8fe961b8b0ca84bccb06980367395d7348fbac93
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 01:05:14 +0300

    add link account + deactivate

--- packages/react-app/src/components/WalletButton.js
+++ packages/react-app/src/components/WalletButton.js
@@ -1,9 +1,31 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+import { shortenAddress, useEthers, useLookupAddress } from '@usedapp/core';
+
+
+import styles from '../styles';
 
 const WalletButton = () => {
-  return (
-    <div>WalletButton</div>
-  )
-}
+  const [accountAddress, setAccountAddress] = useState('');
+  const { ens } = useLookupAddress();
+  const { account, activateBrowserWallet, deactivate } = useEthers();
+  
+  useEffect(() => {
+    if (ens) setAccountAddress(ens);
+    else if (account) setAccountAddress(shortenAddress(account));
+    else setAccountAddress('');
+  }, [account, ens, setAccountAddress]);
 
-export default WalletButton
+  return (
+    <button
+      onClick={() => {
+        if (!account) activateBrowserWallet();
+        else deactivate();
+      }}
+      className={styles.walletButton}>
+      {accountAddress || 'Connect Wallet'}
+    </button>
+  );
+};
+
+export default WalletButton;
