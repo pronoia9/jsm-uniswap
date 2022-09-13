8e9714f3dccfd677a16dbc8dc063fd87406aa4cc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 02:02:00 +0300

    fix fee func

--- packages/react-app/src/utils/getFactoryInfo.js
+++ packages/react-app/src/utils/getFactoryInfo.js
@@ -4,7 +4,7 @@ import { getPairsInfo } from "./getPairsInfo";
 export const getFactoryInfo = async (factoryAddress, web3) => {
   const factory = new web3.eth.Contract(abis.factoryAddress, factoryAddress);
   const factoryInfo = {
-    fee: await factory.methods.fee().call(),
+    fee: await factory.methods.feeTo().call(),
     feeToSetter: await factory.methods.feeToSetter().call(),
     allPairsLength: await factory.methods.all().call(),
     allPairs: [],
