c0b3a2c72456e98c8d97d9e78c0e762decbdc83c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 02:31:07 +0300

    finish ui

--- packages/react-app/src/components/AmountIn.js
+++ packages/react-app/src/components/AmountIn.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
+import { chevronDown } from '../assets';
+import styles from '../styles';
+import { useOnClickOutside } from '../utils';
 
 const AmountIn = () => {
-  return <div>AmountIn</div>;
+  const [showList, setShowList] = useState(false);
+
+  return (
+    <div className={styles.amountContainer}>
+      <input
+        placeholder='0.0'
+        type='number'
+        value=''
+        disabled={false}
+        onChange={() => {}}
+        className={styles.amountInput}
+      />
+
+      <div
+        className='relative'
+        onClick={() => {
+          setShowList((prev) => !prev);
+        }}>
+        <button className={styles.currencyButton}>
+          ETH
+          {/* {activeCurrency} */}
+          <img
+            src={chevronDown}
+            alt='chevron-down'
+            className={`w-3 h-3 object-contain ml-2 ${showList ? 'rotate-180' : 'rotate-0'}`}
+          />
+        </button>
+
+        {showList && (
+          <ul className={styles.currencyList}>
+            {[
+              { token: 'ETH', tokenName: 'ETH' },
+              { token: 'Gold', tokenName: 'Gold' },
+            ].map(({ token, tokenName }, i) => (
+              <li key={i} className={`${styles.currencyListItem}${true ? ' bg-site-dim2' : ''} cursor-pointer`}>
+                {tokenName}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default AmountIn;
