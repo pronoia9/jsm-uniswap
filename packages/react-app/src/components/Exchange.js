bdaf363c4b134bf76249fe5df338f33a1c13ef57
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 02:13:53 +0300

    add layout

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -1,7 +1,20 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import { Contract } from '@ethersproject/contracts';
+import { abis } from '@my-app/contracts';
+import { ERC20, useContractFunction, useEthers, useTokenAllowance, useTokenBalance } from '@usedapp/core';
+import { ethers } from 'ethers';
+import { parseUnits } from 'ethers/lib/utils';
 
-const Exchange = () => {
-  return <div>Exchange</div>;
+import { ROUTER_ADDRESS } from '../config';
+
+const Exchange = ({ pools }) => {
+  return (
+    <div className='flex flex-col w-full items-center'>
+      <div className='mb-8'>
+      </div>
+    </div>
+  );
 };
 
 export default Exchange;
