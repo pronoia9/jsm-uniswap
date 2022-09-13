6be2f84f884de72eed70b12a3fbac8e4aa848bad
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 02:04:49 +0300

    fix feeTo + allPairsLength

--- packages/react-app/src/utils/getFactoryInfo.js
+++ packages/react-app/src/utils/getFactoryInfo.js
@@ -1,16 +1,17 @@
-import { abis } from "@my-app/contracts";
-import { getPairsInfo } from "./getPairsInfo";
+import { abis } from '@my-app/contracts';
+import { getPairsInfo } from './getPairsInfo';
 
 export const getFactoryInfo = async (factoryAddress, web3) => {
-  const factory = new web3.eth.Contract(abis.factoryAddress, factoryAddress);
-  const factoryInfo = {
-    fee: await factory.methods.feeTo().call(),
-    feeToSetter: await factory.methods.feeToSetter().call(),
-    allPairsLength: await factory.methods.all().call(),
-    allPairs: [],
-  }
+  const factory = new web3.eth.Contract(abis.factory, factoryAddress);
 
-  for (let i = 0; i < factoryInfo; i++) {
+  const factoryInfo = {
+    feeTo: await factory.methods.feeTo().call(),
+    feeToSetter: await factory.methods.feeToSetter().call(),
+    allPairsLength: await factory.methods.allPairsLength().call(),
+    allPairs: [],
+  };
+
+  for (let i = 0; i < factoryInfo.allPairsLength; i++) {
     factoryInfo.allPairs[i] = await factory.methods.allPairs(i).call();
   }
 
