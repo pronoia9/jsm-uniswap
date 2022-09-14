548c49b1a862d3008f265fd5506f776e6ae132e1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-15 00:27:18 +0300

    fix typo on alt text

--- packages/react-app/src/components/Loader.js
+++ packages/react-app/src/components/Loader.js
@@ -6,7 +6,7 @@ import { ethereumLogo } from '../assets';
 const Loader = ({ title }) => {
   return (
     <div className={styles.loader}>
-      <img src={ethereumLogo} alt='etherium-logo' className={styles.loaderImg} />
+      <img src={ethereumLogo} alt='ethereum logo' className={styles.loaderImg} />
       <p className={styles.loaderText}>{title}</p>
     </div>
   );
