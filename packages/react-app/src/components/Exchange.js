829b90ed6821b0c7350a0b0ca221b6ba7ef0c75e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 23:03:26 +0300

    add LOTS of states/variables

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -7,7 +7,7 @@ import { ethers } from 'ethers';
 import { parseUnits } from 'ethers/lib/utils';
 
 import { ROUTER_ADDRESS } from '../config';
-import { AmountIn, AmountOut, Balance } from './';
+import { AmountIn, AmountOut, Balance } from '.';
 import styles from '../styles';
 import {
   getAvailableTokens,
@@ -19,6 +19,26 @@ import {
 } from '../utils';
 
 const Exchange = ({ pools }) => {
+  const { account } = useEthers();
+  const [fromValue, setFromValue] = useState('0');
+  const [fromToken, setFromToken] = useState(pools[0].token0Address);
+  const [toToken, setToToken] = useState('');
+  const [resetState, setResetState] = useState(false);
+
+  const fromValueBigNumber = parseUnits(fromValue);
+  const availableTokens = getAvailableTokens(pools);
+  const counterpartTokens = getCounterpartTokens(pools, fromToken);
+  const pairAddress = findPoolByTokens(pools, fromToken, toToken)?.address ?? '';
+
+  const routerContract = new Contract(ROUTER_ADDRESS, abis.router02);
+  const fromTokenContract = new Contract(fromToken, ERC20.abi);
+  const fromTokenBalance = useTokenBalance(fromToken, account);
+  const toTokenBalance = useTokenBalance(toToken, account);
+  const tokenAllowance = useTokenAllowance(fromToken, account, ROUTER_ADDRESS) || parseUnits('0');
+  const approveNeeded = fromValueBigNumber.gt(tokenAllowance);
+  const formValueIsGreaterThan0 = fromValueBigNumber.gt(parseUnits('0'));
+  const hasEnoughBalance = fromValueBigNumber.lte(fromTokenBalance ?? parseUnits('0'));
+
   // !temp
   // const isApproving = isOperationPending('approve'), // TODO
   //   isSwapping = isOperationPending('swap'); // TODO
