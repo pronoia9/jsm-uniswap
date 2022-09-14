9f494dbe3573899a8803b984d71c00a9502277af
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-09-14 22:01:23 +0300

    add initial exchange button

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -19,6 +19,12 @@ import {
 } from '../utils';
 
 const Exchange = ({ pools }) => {
+  // !temp
+  // const isApproving = isOperationPending('approve'), // TODO
+  //   isSwapping = isOperationPending('swap'); // TODO
+  // const successMessage = getSuccessMessage(), // TODO
+  //   failure = getFailureMessage(); // TODO
+
   return (
     <div className='flex flex-col w-full items-center'>
       <div className='mb-8'>
@@ -29,6 +35,26 @@ const Exchange = ({ pools }) => {
         <AmountOut />
         <Balance />
       </div>
+
+      {'approveNeeded' && '!isSwapping' ? (
+        <button
+          disabled={!'canApprove'}
+          onClick={() => {}}
+          className={`${'canApprove' ? 'bg-site-pink text-white' : 'bg-site-dim2 text-site-dim2'} ${
+            styles.actionButton
+          }`}>
+          {'isApproving' ? 'Approving' : 'Approve'}
+        </button>
+      ) : (
+        <button
+          disabled={!'canSwap'}
+          onClick={() => {}}
+          className={`${'canSwap' ? 'bg-site-pink text-white' : 'bg-site-dim2 text-site-dim2'} ${
+            styles.actionButton
+          }`}>
+          {'isSwapping' ? 'Swapping...' : 'hasEnoughBalance'}
+        </button>
+      )}
     </div>
   );
 };
